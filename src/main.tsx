879746718efda0824d8113894723363147559891
meliodas0001 <riquemorozinee@gmail.com>
2024-08-23 15:13:13 -0300

    feat: poppins font added

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.scss";
+
+import "./styles/base/_global.scss";
+import "./styles/base/_reset.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
